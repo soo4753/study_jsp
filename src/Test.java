22a989df5e32d8ee067587c63fd59bf7f5e13785
user <user@DESKTOP-M63NAGB>
2022-07-04 20:19:38 +0900

    commit

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("git test");
 		System.out.println("드디어 commit 성공이다ㅠㅠㅠㅠ");
+		System.out.println("왜 잔디 안 심어져?");
 
 	}
 
